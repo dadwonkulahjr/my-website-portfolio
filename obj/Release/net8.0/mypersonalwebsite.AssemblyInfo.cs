697d00aae44959ded8732dd6de99b11d17b7d67a
dadwonkulahjr <dad.wonkulah@comaxict.com>
2025-11-17 02:56:59 +0100

    Initial-commit-1

--- obj/Release/net8.0/mypersonalwebsite.AssemblyInfo.cs
+++ obj/Release/net8.0/mypersonalwebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mypersonalwebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468177147540a68ab7ea4d1e71308bd5b2f9afc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30afbaad1321ed6b6bd013ef59bf7b50b9cc3b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("mypersonalwebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mypersonalwebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
